ed17f70580d72d0a78f1a6436d53ce3643966bb0
mewhu <mew31716@gmail.com>
2018-06-29 23:52:02 +0800

    tmp commit

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,2 +1,51 @@
 class TodosController < ApplicationController
+  before_action :target_todo, :only => [:show, :edit, :update, :destroy]
+  def index
+    @todos = Todo.all
+  end
+
+  def new
+    @todo = Todo.new
+  end
+
+  def create
+    @todo = Todo.new(todo_params)
+    if @todo.save
+      redirect_to todos_path
+    else
+      render :action => :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @todo.update_attributes(todo_params)
+      redirect_to todos_path
+    else
+      render :action => :edit
+    end
+  end
+
+  def show
+  end
+
+  def destroy
+    @todo.destroy
+    redirect_to todos_path
+  end
+
+  def done
+    
+  end
+
+  private
+  def todo_params
+    params.require(:todo).permit(:name, :due_date, :note)
+  end
+
+  def target_todo
+    @todo = Todo.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,38 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  # 以下的寫法除了為:todos建出基本的7個CRUD動作之外，可以為:is_done做出包含prefix在內都有子母關係的路由
+  # ---------------------------------------------------------------------------------------
+  # Prefix       Verb   URI Pattern                  Controller#Action
+  # is_done_todo PATCH  /todos/:id/is_done(.:format) todos#is_done
+  #        todos GET    /todos(.:format)             todos#index
+  #              POST   /todos(.:format)             todos#create
+  #     new_todo GET    /todos/new(.:format)         todos#new
+  #    edit_todo GET    /todos/:id/edit(.:format)    todos#edit
+  #         todo GET    /todos/:id(.:format)         todos#show
+  #              PATCH  /todos/:id(.:format)         todos#update
+  #              PUT    /todos/:id(.:format)         todos#update
+  #              DELETE /todos/:id(.:format)         todos#destroy
+  # ---------------------------------------------------------------------------------------
+  # resources :todos do
+  #   member do
+  #     patch :is_done
+  #   end
+  # end
+
+
+  # ---------------------------------------------------------------------------------------
+  # Prefix    Verb   URI Pattern                  Controller#Action
+  # todos     GET    /todos(.:format)             todos#index
+  #           POST   /todos(.:format)             todos#create
+  # new_todo  GET    /todos/new(.:format)         todos#new
+  # edit_todo GET    /todos/:id/edit(.:format)    todos#edit
+  # todo      GET    /todos/:id(.:format)         todos#show
+  #           PATCH  /todos/:id(.:format)         todos#update
+  #           PUT    /todos/:id(.:format)         todos#update
+  #           DELETE /todos/:id(.:format)         todos#destroy
+  #           PATCH  /todos/:id/is_done(.:format) todos#done
+  # ---------------------------------------------------------------------------------------
   resources :todos
+  patch "/todos/:id/is_done", to: "todos#done"
 end
